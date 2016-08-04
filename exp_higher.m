3a3e173a05a4a16d2939f9eb8b5af2e74886fcb7
eymenkurdoglu <eymenkurdoglu@gmail.com>
2016-08-04 10:24:25 -0400

    new scripts

--- exp_higher.m
+++
@@ -1,5 +0,0 @@
-function v = exp_higher(k)
-G = length(k)/4;
-k = (reshape(k,4,G))';
-v = ones(G,1) + k(:,2) + k(:,3) + (k(:,3).*k(:,4));
-return
--- mnqt.m
+++
@@ -1,8 +0,0 @@
-function Q = mnqt( t, alpha_t, tmax )
-
-num = 1-exp(-1*alpha_t*(t./tmax).^0.63);
-denom = 1-exp(-1*alpha_t);
-
-Q = num/denom;
-
-return
