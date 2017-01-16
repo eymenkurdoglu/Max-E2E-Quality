134f39413dcd2d19d25a15aab95cefe6cc8ec752
eymenkurdoglu <eymenkurdoglu@gmail.com>
2017-01-16 10:52:41 -0500

    shorter simulation script

--- IID Packet Losses/mnqq.m
+++
@@ -1,8 +0,0 @@
-function Q = mnqq( q, alpha_q, qmin )
-
-num = 1-exp(-1*alpha_q*(qmin./q));
-denom = 1-exp(-1*alpha_q);
-
-Q = num/denom;
-
-return
--- etc/fitmodel.m
+++
@@ -1,5 +0,0 @@
-function y = fitmodel( x, a, b )
-
-y = 1-exp(-1*a*x.^b);
-
-return
